34e004eb0d52e024ddb97585b3e87b45e7c36e31
Barton Phillips <bartonphillips@gmail.com>
2017-04-22 08:17:27 -0700

    Update

--- scripts/bkupdb.sh
+++ scripts/bkupdb.sh
@@ -6,8 +6,8 @@ dir=other
 bkupdate=`date +%B-%d-%y`
 filename="ALLNATURAL_BACKUP.$bkupdate.sql"
 
-mysqldump --user=root --no-data --password=bartonl411 allnatural > $dir/allnatural.schema
-mysqldump --user=root --add-drop-table --password=bartonl411 allnatural >$dir/$filename
+mysqldump --user=root --no-data --password=bartonl411 allnatural 2>/dev/null > $dir/allnatural.schema
+mysqldump --user=root --add-drop-table --password=bartonl411 allnatural 2>/dev/null >$dir/$filename
 
 gzip $dir/$filename
 

5ea6eb96f0b57e91a53ff2e08d4d3871ed5c29f1
Barton Phillips <bartonphillips@gmail.com>
2021-03-23 11:48:04 -0400

    modified:   scripts/bkupdb.sh

--- scripts/bkupdb.sh
+++ scripts/bkupdb.sh
@@ -5,11 +5,11 @@ dir=other
 bkupdate=`date +%B-%d-%y`
 filename="ALLNATURAL_BACKUP.$bkupdate.sql"
 
-mysqldump --defaults-file=/home/barton/ps --user=root --no-data allnatural 2>/dev/null > $dir/allnatural.schema
-mysqldump --defaults-file=/home/barton/ps --user=root --add-drop-table  allnatural 2>/dev/null >$dir/$filename
+mysqldump --defaults-file=/home/barton/ps --user=barton --no-data allnatural 2>/dev/null > $dir/allnatural.schema
+mysqldump --defaults-file=/home/barton/ps --user=barton --add-drop-table  allnatural 2>/dev/null >$dir/$filename
 
 # remove old files
-find $dir -atime +28 -type f -exec rm '{}' \;
+find $dir -mtime +30 -type f -exec rm '{}' \;
 
 gzip $dir/$filename
 

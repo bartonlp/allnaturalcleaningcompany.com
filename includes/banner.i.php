e129e04c887ef6f84842d63301a99a4990ed6602
Barton Phillips <bartonphillips@gmail.com>
2021-07-05 14:55:48 -0400

    move thing around

--- includes/banner.i.php
+++ includes/banner.i.php
@@ -5,21 +5,23 @@
 return <<<EOF
 <header id="header">
 <img src="https://bartonphillips.net/images/allnatural/BubbleLogo.png" alt="bubble-logo">
-<img id="logo" src="https://bartonphillips.net/images/blank.png" alt="blank image">
-<img id='dummyimg' src='/tracker.php?page=normal&id=$this->LAST_ID' alt="blank image">
+<img id="logo" data-image="" src="https://bartonphillips.net/images/blank.png" alt="blank image">
+<img id="dummyimg" src="https://bartonphillips.net/tracker.php?page=noscript&id=$this->LAST_ID">
 <div>All Natural Cleaning Company <span class="phone">Call Us Today: $this->__Phone</span>
 
-<!-- Nav bar for large screens -->
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="residential.php">Residential</a></li>
-    <li><a href="commercial.php">Commercial</a></li>
-    <li><a href="about.php">About Us</a></li>
-    <li><a href="https://allnaturalcleaningcompany.blogspot.com">Blog</a></li>
-    <li><a href="contact.php">Contact</a></li>
-  </ul>
+<!-- Big Nav bar for large screens -->
+  <span id="bignavbar">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="residential.php">Residential</a></li>
+      <li><a href="commercial.php">Commercial</a></li>
+      <li><a href="about.php">About Us</a></li>
+      <li><a href="https://allnaturalcleaningcompany.blogspot.com">Blog</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </span>
 
-<!-- Nav bar for small screens -->
+<!-- Small Nav bar for smaller screens, phones and tablets -->
   <div id="smallnavbar">
     <label for="smallmenu" class="xicon-menu">Menu</label>
     <input type="checkbox" id="smallmenu" role="button">
@@ -44,7 +46,7 @@ html {
 </style>
 <link rel="stylesheet" href="https://bartonphillips.net/css/allnatural/css/allnatural.css"/>
 <p>
-<img src="/tracker.php?page=noscript&id=$this->LAST_ID" alt="noscript image">
+<img src="https://bartonphillips.net/tracker.php?page=noscript&id=$this->LAST_ID" alt="noscript image">
 Your browser either does not support <b>JavaScripts</b> or you have JavaScripts disabled, in either case your browsing
 experience will be significantly impaired. If your browser supports JavaScripts but you have it disabled consider enabaling
 JavaScripts conditionally if your browser supports that. Sorry for the inconvienence.</p>

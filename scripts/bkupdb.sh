6ae1f2cdcc657e767f1e510c6310391b830b6fee
Barton Phillips <bartonphillips@gmail.com>
2021-09-08 16:39:49 -0400

    mod find

--- scripts/bkupdb.sh
+++ scripts/bkupdb.sh
@@ -9,7 +9,7 @@ mysqldump --defaults-file=/home/barton/ps --user=barton --no-data allnatural 2>/
 mysqldump --defaults-file=/home/barton/ps --user=barton --add-drop-table  allnatural 2>/dev/null >$dir/$filename
 
 # remove old files
-find $dir -mtime +30 -type f -exec rm '{}' \;
+find $dir -ctime +30 -type f -exec rm '{}' \;
 
 gzip $dir/$filename
 

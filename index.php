33cb5d2fd483d572175b98eea990e4e48184d807
Barton Phillips <bartonphillips@gmail.com>
2024-01-25 15:41:47 -0500

    removed almost everything

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
-// BLP 2023-09-19 - This site is no longer in use.
-
-$_site = require_once(getenv("SITELOADNAME"));
-
-$S = new SiteClass($_site);
-
-// Updated the phone number in includes/head.i.php
-
-$S->title = "All Natural Cleaning Company";
-
-[$top, $footer] = $S->getPageTopBottom();
-
 echo <<<EOF
-$top
+<!DOCTYPE: html>
+<html>
+<body>
 <hr>
 <h1 style="text-align: center">This site is no longer in business</h1>
 <hr>
-$footer
+</body>
+</html>
 EOF;
